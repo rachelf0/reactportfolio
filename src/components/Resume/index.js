3a6c68ecdd8744f1ae84f2def536c34518705563
Rachel Fritz <rachelfritz0@gmail.com>
2020-10-16 10:39:19 -0500

    updates

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,8 +4,9 @@ function Resume() {
 
     return (
         <section id="resume">
-            <a href="./assets/RachelFritzResume.pdf">Resume</a>
+            <a href="https://docs.google.com/document/d/1019J-jNZGOaDDVJYdEWB0UTNVQV0PYF0o-YEMQ7HPmg/edit?usp=sharing" download>
                 Click here to download a copy of my resume.
+            </a>
         </section>
     );
 
